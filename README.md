9e99da7aef5b785d9224624fc8a9519553b7f84a
Abel Darío Rangel Silva <ransilad@gmail.com>
2018-06-09 19:15:06 -0400

    Practica inicial, configurar gulp con sass, browser-sync, useref, uglify, cssnano, del y sequence

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# gulp-practice
-Prácticas de sass con gulp (3.9.1)
+npm install gulp --save-dev
+
+Para compilar Sass
+
+npm install gulp-sass --save-dev
+
+La sincronización del navegador ayuda a facilitar el desarrollo de la web mediante la creación de un servidor web que nos ayuda a hacer una recarga en tiempo real.
+
+npm install browser-sync --save-dev
+
+Para unificar muchos estilos o js en uno solo
+
+npm install gulp-useref --save-dev
+
+Para minificar el JS
+
+npm install gulp-uglify --save-dev
+
+Para minificar el css
+
+npm install gulp-cssnano
+
+Como estamos generando archivos automáticamente, querremos asegurarnos de que los archivos que ya no se usan no permanezcan en ningún lado sin que lo sepamos.
+
+npm install del --save-dev
+
+Para correr tareas en secencia
+
+npm install run-sequence --save-dev
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+// minificar JS
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+// minificar css
+var cssnano = require('gulp-cssnano');
+// Limpiar
+var del = require('del');
+// para correr tareas en secuencias
+var runSequence = require('run-sequence');
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+  .pipe(sass()) // Using gulp-sass
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+  .pipe(useref())
+  // Minifies only if it's a JavaScript file
+  .pipe(gulpIf('*.js', uglify()))
+  // Minifies only if it's a CSS file
+  .pipe(gulpIf('*.css', cssnano()))
+  .pipe(gulp.dest('dist'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean', function() {
+  return del.sync('dist');
+});
+
+gulp.task('build', function (callback){
+  runSequence('clean', ['sass', 'useref', 'fonts'], callback);
+  console.log('Building files');
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass', 'browserSync', 'watch'], callback);
+});
+
+// Para cambiar dinamicamente los archivos que se cambian segun el src
+// gulp.watch('app/scss/**/*.scss', ['sass']); 
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
